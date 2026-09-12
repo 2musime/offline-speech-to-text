2f93ecc7d3081ef0b27e4b866aa4d77a54de77f7
David TUMUSIME <tumusimedavid14@gmail.com>
2026-09-12 11:34:00 +0200

    Stop tracking generated build files

--- build/third_party/whisper.cpp/ggml/src/CMakeFiles/ggml-cpu.dir/ggml-cpu/hbm.cpp.o.d
+++
@@ -1,3 +0,0 @@
-third_party/whisper.cpp/ggml/src/CMakeFiles/ggml-cpu.dir/ggml-cpu/hbm.cpp.o: \
- /home/musime/projects/personal/speech-to-text-app/third_party/whisper.cpp/ggml/src/ggml-cpu/hbm.cpp \
- /usr/include/stdc-predef.h
